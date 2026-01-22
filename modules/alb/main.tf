4cb772469c014beda986e527a2a11cdf340c50f8
jlsalvador-stemdo <jlsalvador@stemdo.io>
2026-01-22 12:17:37 +0100

    Modify ALB configuration with updated protocols
    Updated security group protocol and listener protocol variables for ALB configuration.

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -14,7 +14,7 @@ resource "aws_security_group" "this" {
     ingress {
         from_port   = var.port_ingress
         to_port     = var.port_ingress
-        protocol    = var.ingress_protocol
+        protocol    = var.security_group_ingress_protocol
         cidr_blocks = var.allowed_cidr_blocks_ingress
     }
 
@@ -29,7 +29,7 @@ resource "aws_security_group" "this" {
 resource "aws_lb_target_group" "this" {
     name     = "${var.project_name}-tg"
     port     = var.port_ingress
-    protocol = var.ingress_protocol
+    protocol = var.listener_protocol
     vpc_id   = var.vpc_id
 }
 
@@ -37,10 +37,11 @@ resource "aws_lb_listener" "this" {
     load_balancer_arn = aws_lb.this.arn 
 
     port              = var.port_ingress
-    protocol          = var.ingress_protocol
+    protocol          = var.listener_protocol
 
     default_action {
         type             = "forward"
         target_group_arn = aws_lb_target_group.this.arn
     }
+
 }
